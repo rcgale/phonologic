d09b495883357860f283e5b411789991f22eced3
rcgale <rcgale@gmail.com>
2022-03-23 15:35:17 -0700

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='phonologic',
-    version='0.0.0',
+    version='0.0.1',
     author='Robert Gale',
     author_email='galer@ohsu.edu',
     packages=[
@@ -20,7 +20,7 @@ setuptools.setup(
     package_data={
         'phonologic': [
             '**/*.py',
-            '**/*.txt',
+            '**/*.phl',
             '**/*.json',
         ]
     },
