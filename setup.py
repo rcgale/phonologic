186e56f6408ecd57950628d207bf5b1fdc52d1fc
rcgale <rcgale@gmail.com>
2022-11-13 02:30:58 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='phonologic',
-    version='0.3.0',
+    version='0.3.1',
     author='Robert Gale',
     author_email='galer@ohsu.edu',
     packages=setuptools.find_packages(),
